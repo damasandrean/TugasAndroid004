a614858249f60033c69d19bc0fca275b57569575
DAMAS ANDREAN <andreandamas@gmail.com>
2019-09-23 19:42:35 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -0,0 +1,33 @@
+package id.ac.polinema.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileBundleActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_bundle);
+
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            usernameText.setText(extras.getString("username"));
+            nameText.setText(extras.getString("name"));
+            ageText.setText(String.valueOf(getIntent().getIntExtra("age", 0)));
+        }
+    }
+}
